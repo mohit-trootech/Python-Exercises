42bb7ee5ebd2f76a42e60cdd92996a1647ccbb05
= <mohit.prajapat@trootech.com>
2024-07-03 16:42:39 +0530

    [jupyter] Update

---
+++ Datatypes Python/dictionary_main.py
@@ -0,0 +1,6 @@
+"""Dictionary: in python comprises an unordered collection of data stored in a key: value format. Dictionary is also
+known as "Associative memories" or "Associative arrays". In a dictionary, the key must always be unique. Whereas the
+values can be repeated and can be of any data type. The key-value pair should be represented as "key: value" (: colon
+is a must)."""
+
+#
